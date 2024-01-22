197b7beecc656e42fcd6b0af42d2d31fa33dcb59
MartinMGomezVega <martin_gomezvega@hotmail.com>
2024-01-21 23:57:41 -0300

    Actualizado

--- routers/uploadTransactionFile.go
+++ routers/uploadTransactionFile.go
@@ -8,9 +8,9 @@ import (
 
 	"github.com/MartinMGomezVega/Tech_Challenge/models"
 	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go/aws"
 )
 
 type AWSService struct {
@@ -24,50 +24,56 @@ func UploadTransactionFile(ctx context.Context, request events.APIGatewayProxyRe
 	bucketName := ctx.Value(models.Key("bucketName")).(string)
 	log.Println("bucket: " + bucketName)
 
-	body := ctx.Value(models.Key("body")).(string)
-	log.Println("body: " + body)
+	// Ruta completa al archivo que deseas subir
+	filePath := "C:\\Users\\Martin\\Tech_Challenge\\files\\20417027050.csv"
 
 	config, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-1"))
 	if err != nil {
 		log.Println("Error while loading the aws config: ", err)
 		r.Status = 400
 		r.Message = "Error while loading the aws config."
+		return r
 	}
 
 	AWSService := AWSService{
 		S3Client: s3.NewFromConfig(config),
 	}
 
-	r = AWSService.UploadFile(bucketName, "test", "/files/20417027050.csv")
+	// Utilizando el nombre del archivo como bucketKey
+	r = AWSService.UploadFile(bucketName, "20417027050.csv", filePath)
 
 	return r
 }
 
-func (awsSvc AWSService) UploadFile(bucketName string, bucketKey string, fileName string) models.ResposeAPI {
+func (awsSvc AWSService) UploadFile(bucketName string, bucketKey string, filePath string) models.ResposeAPI {
 	var r models.ResposeAPI
-	file, err := os.Open(fileName)
+
+	// Abrir el archivo
+	file, err := os.Open(filePath)
 	if err != nil {
-		fmt.Println(fmt.Errorf("failed to open file %q, %v", fileName, err))
+		fmt.Println(fmt.Errorf("failed to open file %q, %v", filePath, err))
 		r.Status = 400
 		r.Message = "Failed to open file."
-	} else {
-		defer file.Close()
-		// Upload the file to S3.
-		result, err := awsSvc.S3Client.PutObject(context.TODO(), &s3.PutObjectInput{
-			Bucket: aws.String(bucketName),
-			Key:    aws.String(bucketKey),
-			Body:   file,
-		})
-		if err != nil {
-			fmt.Println(fmt.Errorf("failed to upload file, %v", err))
-			r.Status = 400
-			r.Message = "Failed to upload file."
-		} else {
-			r.Status = 200
-			r.Message = "CSV file successfully uploaded."
-		}
-		fmt.Println(result)
+		return r
 	}
+	defer file.Close()
+
+	// Subir el contenido del archivo a S3
+	result, err := awsSvc.S3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(bucketKey),
+		Body:   file,
+	})
+	if err != nil {
+		fmt.Println(fmt.Errorf("failed to upload file, %v", err))
+		r.Status = 400
+		r.Message = "Failed to upload file."
+		return r
+	}
+
+	r.Status = 200
+	r.Message = "CSV file successfully uploaded."
+	fmt.Println(result)
 
 	return r
 }
