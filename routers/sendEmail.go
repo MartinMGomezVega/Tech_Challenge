942abf25875ef60ff9b5f0066565806f6e8e4cbb
MartinMGomezVega <martin_gomezvega@hotmail.com>
2024-01-22 21:10:25 -0300

    Actualizado

--- routers/sendEmail.go
+++ routers/sendEmail.go
@@ -75,12 +75,12 @@ func SendEmail(ctx context.Context, request events.APIGatewayProxyRequest) model
 	}
 
 	// info extra
-	bodyEmail += "<br><br><br>¡Si surgen preguntas, estamos aquí para ayudarte! <br>Explora nuestras Preguntas Frecuentes en www.storicard.com/preguntas-frecuentes para obtener respuestas rápidas y útiles. ¡Tu tranquilidad es nuestra prioridad!<br>"
-	bodyEmail += "Conoce más sobre nosotros en nuestro sitio web: www.storicard.com<br><br>"
+	bodyEmail += "<br><br><br>¡Si surgen preguntas, estamos aquí para ayudarte! <br>Explora nuestras Preguntas Frecuentes en <a href='www.storicard.com/preguntas-frecuentes'>www.storicard.com/preguntas-frecuentes</a> para obtener respuestas rápidas y útiles. ¡Tu tranquilidad es nuestra prioridad!<br>"
+	bodyEmail += "Conoce más sobre nosotros en nuestro sitio web: <a href='www.storicard.com'>www.storicard.com</a><br><br>"
 
 	// Add the image to the background of the email
 	// bodyEmail += `<img src="../img/stori_logo.png" style="position: fixed; bottom: 0; right: 0; width: 100%; height: auto;">`
-	bodyEmail += `<br><a href="https://www.storicard.com/"><img src="https://upload.wikimedia.org/wikipedia/commons/b/b0/Stori_Logo_2023.svg" alt="Texto Alternativo"></a>`
+	bodyEmail += `<br><img src="https://upload.wikimedia.org/wikipedia/commons/b/b0/Stori_Logo_2023.svg" alt="stori" style="width: 30%; height: auto;">`
 
 	// Email subject
 	subject := "Stori - Resumen"
